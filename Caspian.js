df7d50edfa8d5a434990b25396e4946df819ceb6
sepehr-gamer <147719785+sepehr-gamer@users.noreply.github.com>
2026-09-21 14:11:49 +0330

    Update Caspian.js

--- Caspian.js
+++ Caspian.js
@@ -10823,7 +10823,7 @@ async function testUserSocksProxy() {
 				window.location.reload();
 			}
 		}
-const CURRENT_VERSION = '3.0.0';
+const CURRENT_VERSION = '3.0.1';
 const UPDATE_FIX = "constsCURRENT_VERSION='d.d.d'";
 		async function checkForUpdates(isManual = false) {
 			try {
