bb97a8b78e8e1286efa99b0c58094c6a6e5fc825
sepehr-gamer <147719785+sepehr-gamer@users.noreply.github.com>
2026-09-24 09:47:41 +0330

    Update Caspian.js

--- Caspian.js
+++ Caspian.js
@@ -80,7 +80,7 @@ async function fetchUpdateSource(path, options = {}) {
 	const url = `https://raw.githubusercontent.com/sepehr-gamer/Caspian-pannel/main/${path}`;
 	return await fetch(url, options);
 }
-const PANEL_VERSION = "5.2.0";
+const PANEL_VERSION = "5.3.0";
 const TEHRAN_OFFSET_MS = (3 * 60 + 30) * 60 * 1000;
 const DAILY_RESET_HOUR = 3;
 const DAILY_RESET_MINUTE = 30;
@@ -12927,7 +12927,7 @@ async function testUserSocksProxy() {
 				window.location.reload();
 			}
 		}
-const CURRENT_VERSION = '5.2.0';
+const CURRENT_VERSION = '5.3.0';
 const UPDATE_FIX = "constsCURRENT_VERSION='d.d.d'";
 		async function checkForUpdates(isManual = false) {
 			try {
